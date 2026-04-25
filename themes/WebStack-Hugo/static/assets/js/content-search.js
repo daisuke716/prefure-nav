3ce4f5bfbe99487e0631815cf4548cb49345563f
daisuke716 <adaisuke716@gmail.com>
2026-04-25 15:07:57 +0000

    auto select baidu and google

--- themes/WebStack-Hugo/static/assets/js/content-search.js
+++ themes/WebStack-Hugo/static/assets/js/content-search.js
@@ -1,6 +1,49 @@
 //关键词sug
 var hotList = 0;
+var suggestionEngine = null; // 'baidu' | 'google'
+
+function initSuggestionEngine() {
+    var cached = localStorage.getItem('sg_country');
+    var cachedTime = parseInt(localStorage.getItem('sg_country_time') || '0');
+    // 缓存24小时
+    if (cached && (Date.now() - cachedTime) < 86400000) {
+        suggestionEngine = (cached === 'CN') ? 'baidu' : 'google';
+        return;
+    }
+    $.getJSON('https://ipapi.co/json/', function(data) {
+        var country = data.country_code || 'XX';
+        localStorage.setItem('sg_country', country);
+        localStorage.setItem('sg_country_time', Date.now().toString());
+        suggestionEngine = (country === 'CN') ? 'baidu' : 'google';
+    }).fail(function() {
+        suggestionEngine = 'google';
+    });
+}
+
+function fetchSuggestions(keywords, onSuccess) {
+    var engine = suggestionEngine || 'google';
+    if (engine === 'baidu') {
+        $.ajax({
+            url: 'https://suggestion.baidu.com/su?wd=' + encodeURIComponent(keywords),
+            dataType: 'jsonp',
+            jsonp: 'cb',
+            success: function(res) { onSuccess(res.s || []); },
+            error: function() { onSuccess([]); }
+        });
+    } else {
+        $.ajax({
+            url: 'https://suggestqueries.google.com/complete/search?client=chrome&q=' + encodeURIComponent(keywords),
+            dataType: 'jsonp',
+            jsonp: 'callback',
+            success: function(res) { onSuccess(res[1] || []); },
+            error: function() { onSuccess([]); }
+        });
+    }
+}
+
 $(function() {
+    initSuggestionEngine();
+
     var $word = $('#word');
     var $container = $word.closest('.search-smart-tips');
 
@@ -15,46 +58,30 @@ $(function() {
 
     $('#search-text').keyup(function() {
         var keywords = $(this).val();
-        if (keywords == '') { hideSuggestions(); return };
-        $.ajax({
-            url: 'https://suggestqueries.google.com/complete/search?client=chrome&q=' + encodeURIComponent(keywords),
-            dataType: 'jsonp',
-            jsonp: 'callback',
-            success: function(res) {
-                $word.empty();
-                var suggestions = res[1];
-                hotList = suggestions.length;
-                if (hotList) {
-                    for (var i = 0; i < hotList; i++) {
-                        // $word.append("<li><span>" + (i + 1) + "</span>" + suggestions[i] + "</li>");
-                        $word.append("<li>" + suggestions[i] + "</li>");
-                        // if (i === 0) {
-                        //     $word.find("li:last span").css({"color": "#fff", "background": "#f54545"});
-                        // } else if (i === 1) {
-                        //     $word.find("li:last span").css({"color": "#fff", "background": "#ff8547"});
-                        // } else if (i === 2) {
-                        //     $word.find("li:last span").css({"color": "#fff", "background": "#ffac38"});
-                        // }
-                    }
-                    showSuggestions();
-                } else {
-                    hideSuggestions();
+        if (keywords == '') { hideSuggestions(); return; }
+        fetchSuggestions(keywords, function(suggestions) {
+            $word.empty();
+            hotList = suggestions.length;
+            if (hotList) {
+                for (var i = 0; i < hotList; i++) {
+                    // $word.append("<li><span>" + (i + 1) + "</span>" + suggestions[i] + "</li>");
+                    $word.append("<li>" + suggestions[i] + "</li>");
                 }
-            },
-            error: function() {
+                showSuggestions();
+            } else {
                 hideSuggestions();
             }
-        })
-    })
+        });
+    });
 
     $(document).on('click', '#word li', function() {
         var word = $(this).text().replace(/^[0-9]/, '');
         $('#search-text').val(word);
         hideSuggestions();
         $('.submit').trigger('click');
-    })
+    });
 
     $(document).on('click', '.io-grey-mode', function() {
         hideSuggestions();
-    })
+    });
 })
